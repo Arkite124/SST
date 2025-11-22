d9f0e7a9622f988e9e5123ffe624f7447c0861a6
EuncheaLee <okwbbg@gmail.com>
2025-11-23 01:02:06 +0900

    251122: Test 페이지 수정

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -7,7 +7,7 @@ import {ToastContainer} from "react-toastify";
 
 export default function MainLayout() {
     return (
-        <div className="flex flex-col min-h-screen bg-[#F6F6F6] text-[#263035]">
+        <div className="flex flex-col min-h-screen bg-[#ffffff] text-[#263035]">
             {/* 헤더를 fixed로 만들고 z-index 적용 */}
             <Header className="fixed top-0 left-0 w-full z-50 "/>
             {/* main에 padding-top으로 header 높이 만큼 공간 확보 */}
--- frontend/src/pages/Main/section/Section1.jsx
+++ frontend/src/pages/Main/section/Section1.jsx
@@ -5,13 +5,13 @@ import cloud from "@/assets/cuteCloud.png"
 export default function Section1() {
         return (
             <>
+                {/* 배경 아래의 흰 줄은 이미지 자체의 문제이므로 자를 예정입니다. */}
                 <section
                     className="text-center backdrop-blur-md shadow-inner snap-start
                  flex flex-col items-center justify-center
-                 bg-repeat-y bg-[length:auto] bg-center"
+                 bg-no-repeat bg-cover bg-center h-[100vh] border-none"
                     style={{
                         width: "100%",
-                        height: "calc(100vh)",
                         backgroundImage: `url(${bgImg})`,
                     }}
                 >
@@ -20,7 +20,7 @@ export default function Section1() {
                         <img
                             src={cloud}
                             alt="cloud"
-                            className="absolute w-[20%] mb-[5%] ml-[10%] cloud-animation"
+                            className="absolute max-w-[250px] w-[20%] bottom-[50%] left-[40%] cloud-animation"
                         />
                     </div>
                 </section>
